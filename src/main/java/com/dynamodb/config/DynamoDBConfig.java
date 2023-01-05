0616aad4bcacdb16390e2a5e889d75f53f454676
namrahov <amrahovnurlan@gmail.com>
2023-01-06 01:25:02 +0400

    11th

--- src/main/java/com/dynamodb/config/DynamoDBConfig.java
+++ src/main/java/com/dynamodb/config/DynamoDBConfig.java
@@ -58,29 +58,114 @@ public class DynamoDBConfig {
                 .build();
         DynamoDB dynamoDB = new DynamoDB(client);
 
-        List<AttributeDefinition> attributeDefinitions = new ArrayList<>();
-        attributeDefinitions.add(new AttributeDefinition().withAttributeName("id").withAttributeType("S"));
 
-        List<KeySchemaElement> keySchema = new ArrayList<>();
-        keySchema.add(new KeySchemaElement().withAttributeName("id").withKeyType(KeyType.HASH));
 
-        CreateTableRequest request = new CreateTableRequest()
+
+        List<AttributeDefinition> attributeDefinitionsEmployee = new ArrayList<>();
+        attributeDefinitionsEmployee.add(new AttributeDefinition().withAttributeName("id").withAttributeType("S"));
+
+        List<KeySchemaElement> keySchemaEmployee = new ArrayList<>();
+        keySchemaEmployee.add(new KeySchemaElement().withAttributeName("id").withKeyType(KeyType.HASH));
+
+        CreateTableRequest requestEmployee = new CreateTableRequest()
                 .withTableName("employee")
-                .withKeySchema(keySchema)
-                .withAttributeDefinitions(attributeDefinitions)
+                .withKeySchema(keySchemaEmployee)
+                .withAttributeDefinitions(attributeDefinitionsEmployee)
                 .withProvisionedThroughput(new ProvisionedThroughput()
                         .withReadCapacityUnits(5L)
                         .withWriteCapacityUnits(6L));
+
+
+
+        List<AttributeDefinition> attributeDefinitionsApplication = new ArrayList<>();
+        attributeDefinitionsApplication.add(new AttributeDefinition().withAttributeName("ApplicationId").withAttributeType("S"));
+
+        List<KeySchemaElement> keySchemaApplication = new ArrayList<>();
+        keySchemaApplication.add(new KeySchemaElement().withAttributeName("ApplicationId").withKeyType(KeyType.HASH));
+
+        CreateTableRequest requestApplication = new CreateTableRequest()
+                .withTableName("application")
+                .withKeySchema(keySchemaApplication)
+                .withAttributeDefinitions(attributeDefinitionsApplication)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+
+
+
+        List<AttributeDefinition> attributeDefinitionsStatusHistory = new ArrayList<>();
+        attributeDefinitionsStatusHistory.add(new AttributeDefinition().withAttributeName("StatusHistoryId").withAttributeType("S"));
+
+        List<KeySchemaElement> keySchemaStatusHistory = new ArrayList<>();
+        keySchemaStatusHistory.add(new KeySchemaElement().withAttributeName("StatusHistoryId").withKeyType(KeyType.HASH));
+
+        CreateTableRequest requestStatusHistory = new CreateTableRequest()
+                .withTableName("status_history")
+                .withKeySchema(keySchemaStatusHistory)
+                .withAttributeDefinitions(attributeDefinitionsStatusHistory)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+
+
+
+        List<AttributeDefinition> attributeDefinitionsMovie = new ArrayList<>();
+        attributeDefinitionsMovie.add(new AttributeDefinition().withAttributeName("filmId").withAttributeType("S"));
+
+        List<KeySchemaElement> keySchemaMovie = new ArrayList<>();
+        keySchemaMovie.add(new KeySchemaElement().withAttributeName("filmId").withKeyType(KeyType.HASH));
+
+        CreateTableRequest requestMovie = new CreateTableRequest()
+                .withTableName("hesen")
+                .withKeySchema(keySchemaMovie)
+                .withAttributeDefinitions(attributeDefinitionsMovie)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+
+
+
+        List<AttributeDefinition> attributeDefinitionsApp = new ArrayList<>();
+        attributeDefinitionsApp.add(new AttributeDefinition().withAttributeName("id").withAttributeType("S"));
+
+        List<KeySchemaElement> keySchemaApp = new ArrayList<>();
+        keySchemaApp.add(new KeySchemaElement().withAttributeName("id").withKeyType(KeyType.HASH));
+
+        CreateTableRequest requestApp = new CreateTableRequest()
+                .withTableName("app")
+                .withKeySchema(keySchemaApp)
+                .withAttributeDefinitions(attributeDefinitionsApp)
+                .withProvisionedThroughput(new ProvisionedThroughput()
+                        .withReadCapacityUnits(5L)
+                        .withWriteCapacityUnits(6L));
+
 
         Table table;
 
         try {
             if (!isTableExist(dynamoDB.getTable("employee"))) {
-                table = dynamoDB.createTable(request);
+                table = dynamoDB.createTable(requestEmployee);
                 table.waitForActive();
             }
+            if (!isTableExist(dynamoDB.getTable("application"))) {
+                table = dynamoDB.createTable(requestApplication);
+                table.waitForActive();
+            }
+            if (!isTableExist(dynamoDB.getTable("status_history"))) {
+                table = dynamoDB.createTable(requestStatusHistory);
+                table.waitForActive();
+            }
+            if (!isTableExist(dynamoDB.getTable("hesen"))) {
+                table = dynamoDB.createTable(requestMovie);
+                table.waitForActive();
+            }
+            if (!isTableExist(dynamoDB.getTable("app"))) {
+                table = dynamoDB.createTable(requestApp);
+                table.waitForActive();
+            }
+
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            System.out.println("postConstruct InterruptedException");
         }
     }
 
